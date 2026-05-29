c96a0a1703d7c932d664a8cf71fb49c861463faa
sachinbudhamagar <sachinbmagar19@gmail.com>
2026-05-29 19:44:42 +0900

    Add Docker support with Dockerfile and docker-compose.yml; update .gitignore and settings

--- AI_JobHunter/settings.py
+++ AI_JobHunter/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-z8#i71mx+xwik1g3xiz(e2$ngr#t+o#i2lnoc0ll5nr_8rqbdy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -116,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
